7649253149f05db56b3478f9b95f7b6f50c2c472
Matthias Richter <vrld@vrld.org>
2024-06-16 21:25:06 +0200

    SMD diode only

--- footprints/diode.js
+++ footprints/diode.js
@@ -2,45 +2,36 @@ module.exports = {
   params: {
     designator: 'D',
     from: undefined,
-    to: undefined
+    to: undefined,
+    side: 'F',
   },
-  body: p => `
-  
-    (module ComboDiode (layer F.Cu) (tedit 5B24D78E)
+  body: p => {
+    const anode_pos = p.side == 'F' ? .3 : -.3;
+    const diode_triangle = `
+      (fp_line (start ${-anode_pos} -.3) (end ${-anode_pos} .3) (layer ${p.side}.SilkS) (width .1))
+      (fp_poly (pts (xy ${-anode_pos} 0) (xy ${anode_pos} .3) (xy ${anode_pos} -.3)) (layer ${p.side}.SilkS) (width .1) (fill none))
+    `;
 
+    const anode = p.side == 'F' ? p.from.str : p.to.str;
+    const cathode = p.side == 'F' ? p.to.str : p.from.str;
 
+    return `
+    (module lib:smd_diode (layer F.Cu) (tedit 5B24D78E)
         ${p.at /* parametric position */}
 
         ${'' /* footprint reference */}
-        (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-        (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
-        
+        (fp_text reference "${p.ref}" (at 0 0) (layer ${p.side}.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+        (fp_text value "" (at 0 0) (layer ${p.side}.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+
         ${''/* diode symbols */}
-        (fp_line (start 0.25 0) (end 0.75 0) (layer F.SilkS) (width 0.1))
-        (fp_line (start 0.25 0.4) (end -0.35 0) (layer F.SilkS) (width 0.1))
-        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 0.55) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.75 0) (end -0.35 0) (layer F.SilkS) (width 0.1))
-        (fp_line (start 0.25 0) (end 0.75 0) (layer B.SilkS) (width 0.1))
-        (fp_line (start 0.25 0.4) (end -0.35 0) (layer B.SilkS) (width 0.1))
-        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
-    
+        (fp_line (start -.8 0) (end -.3 0) (layer ${p.side}.SilkS) (width .1))
+        (fp_line (start .3 0) (end .8 0) (layer ${p.side}.SilkS) (width .1))
+        ${diode_triangle}
+
         ${''/* SMD pads on both sides */}
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.from.str})
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.from.str})
-        
-        ${''/* vias */}
-        (pad 1 thru_hole circle (at -0.75 0 ${p.rot}) (size 0.6 0.6) (drill 0.3) (layers *.Cu *.Mask) ${p.to.str})
-        (pad 2 thru_hole circle (at  0.75 0 ${p.rot}) (size 0.6 0.6) (drill 0.3) (layers *.Cu *.Mask) ${p.from.str})
+        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${cathode})
+        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${anode})
     )
-  
     `
+  }
 }
